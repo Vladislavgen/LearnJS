ba76ebc7db48814ea61cbc1be3be8bb045f70735
ThunderUSK <daria_usk@mail.ru>
2018-08-05 14:14:33 +0300

    JS за час

---
+++ script.js
@@ -0,0 +1,14 @@
+//jQuery("body").append("<a href='http://google.com'>Перейти в гугл</a>");
+
+//var p_block;
+//p_block = jQuery('p').clone();
+
+//jQuery("body").append(p_block);
+
+jQuery("document").ready(function () {
+jQuery("button").on("click", function () {
+    alert("Кнопка ")
+});
+
+
+});
